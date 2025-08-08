1f22fee49044791e42a3de621caaf8f9ae7af198
Hannah Goodridge <Hannah-goodridge@users.noreply.github.com>
2025-08-08 23:53:17 +0100

    Enable marketing only for me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 'use client'
 
 import MarketingPage from './marketing/page'
+import WhatsOnPage from './whats-on/page'
 
-export default MarketingPage
+export default function Index() {
+  const enableMarketing = process.env.NEXT_PUBLIC_ENABLE_MARKETING === 'true'
+  return enableMarketing ? <MarketingPage /> : <WhatsOnPage />
+}
